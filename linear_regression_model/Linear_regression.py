88bb1eb29747e0f469a39a48c71ff027c289a477
rath772k <45796608+rath772k@users.noreply.github.com>
2018-12-19 12:31:26 +0530

    Update Linear_regression.py

--- linear_regression_model/Linear_regression.py
+++ linear_regression_model/Linear_regression.py
@@ -23,11 +23,12 @@ class lin_reg:
     def cost_fn(self):                         #mean squared error
         return np.sum(np.square(self.x.dot(self.w)+self.b-self.y))/2/np.size(self.y)+lambda_/2/np.size(self.y)*np.square(self.w)
     
-    def grad_descent(self,l_rate,no_of_iter,lambda_=0):  #gradient descent
+  def grad_descent(self,l_rate,no_of_iter,lambda_=0):  #gradient descent
         m=np.size(self.y)
         for i in range(no_of_iter):
-            self.w -= l_rate / m * self.x.T.dot(self.x.dot(self.w) + self.b - self.y) + lambda_ / m * self.w
-            self.b -= np.sum(l_rate / m * (self.x.dot(self.w) + self.b - self.y))
+            dif=self.x.dot(self.w)+self.b-self.y
+            self.w -= l_rate / m * self.x.T.dot(dif) + lambda_ / m * self.w
+            self.b -= np.sum(l_rate / m * dif)
         return self.w,self.b
     
     def predict(self,xin):        #predicts results for the test data
